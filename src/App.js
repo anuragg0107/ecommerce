0c4af77b28dbb557a4b5bfb7a710d8882e6b3028
anurag gupta <101393446+anuragg0107@users.noreply.github.com>
2023-12-29 16:24:08 +0530

    Error page create

--- src/App.js
+++ src/App.js
@@ -2,33 +2,20 @@ import React from 'react';
 import Home from './pages/Home/Home';
 import LoginPage from './pages/Login/LoginPage';
 import SignupPage from './pages/Signup/SignupPage';
-// import './App.css';
-import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import "./index.css";
+import Error from './pages/Error/Error';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './index.css';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home></Home>,
-  },
-  {
-    path: "/Login",
-    element: <LoginPage></LoginPage>,
-  },
-  {
-    path: "/SignUp",
-    element: <SignupPage></SignupPage>,
-  },
-]);
-function App() {
+const App = () => {
   return (
-    <div>
-   <RouterProvider router={router} />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<LoginPage />} />
+        <Route path="/SignUp" element={<SignupPage />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
   );
 }
 
